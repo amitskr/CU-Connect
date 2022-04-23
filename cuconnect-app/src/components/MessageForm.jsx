b8afd203f552be620e431b27ad667a5fcdda480e
amitskr <amitskr6@gmail.com>
2022-04-23 13:20:02 +0530

    Save 2
    Complete status 43:20
    Picture icon added.

--- cuconnect-app/src/components/MessageForm.jsx
+++ cuconnect-app/src/components/MessageForm.jsx
@@ -1,8 +1,42 @@
-const MessageForm = () => {
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+
+const MessageForm = (props) => {
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds, chatId, { text});
+
+        setValue('');
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+
+        isTyping(props, chatId);
+    }
+
     return(
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon"/>
+                </span>
+            </label>
+        </form>
     );
 }
 
--- cuconnect-app/src/components/MyMessage.jsx
+++ cuconnect-app/src/components/MyMessage.jsx
@@ -1,7 +1,18 @@
-const MYMessage = () => {
+const MYMessage = ({ message }) => {
+   if(message?.attachments?.length > 0) {
+       return (
+           <img
+               src={message.attachments[0].file}
+               alt="message-attachemnt"
+               className="message-image"
+               style={{ float: "right"}}
+           />
+       )
+   }
+
     return(
-        <div>
-            MYMessage
+        <div className="message" style={{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+            {message.text}
         </div>
     );
 }
